98389351f01f0564b2af097b5170cd345632de69
marina6coneto <santoscmarina@hotmail.com>
2023-11-08 09:19:23 -0300

    Atualizações em aula22.py e adição dos arquivos aula23.py e aula24.py

--- aula22.py
+++ aula22.py
@@ -8,3 +8,14 @@
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
+
+
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+# senha_permitida = '123456'
+# if (entrada == 'E' or entrada  == 'e') and senha_digitada == senha_permitida:
+#      print('Entrar')
+# else: 
+#      print('Sair')
+    
+print (True or False or 0 or 'abc')
