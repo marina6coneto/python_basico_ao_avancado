ce508a45c5ccc0b1358299373673d6e181698405
marina6coneto <santoscmarina@hotmail.com>
2024-03-19 15:00:23 -0300

    log

--- aula133/log.py
+++ aula133/log.py
@@ -1,5 +1,10 @@
 #Abstração 
 #Herança - é um (ex: LogFileMix é um Log)
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
@@ -12,7 +17,11 @@ class Log:
     
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})' 
+        print('Salvando no log...', msg_formatada)
+        with open(LOG_FILE, 'a') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
     
 class LogPrintMixin(Log):
     def _log(self, msg):
@@ -20,8 +29,15 @@ class LogPrintMixin(Log):
     
     
 if __name__ == '__main__':
-    l = LogPrintMixin()
-    l._log('Qualquer Coisa')
-    l.log_error('avae')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp._log('Qualquer Coisa')
+    lp.log_error('avae')
+    lp.log_success('Que legal')
+    
+    lf = LogFileMixin()
+    lf._log('Qualquer Coisa')
+    lf.log_error('avae')
+    lf.log_success('Que legal')
+
+    
     
