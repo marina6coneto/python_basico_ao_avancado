f3600aeb99031601ca1d21bf3786835b8d95f177
marina6coneto <santoscmarina@hotmail.com>
2024-03-20 13:50:42 -0300

    Polimorfismo

--- aula136.py
+++ aula136.py
@@ -11,3 +11,37 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+
+from abc import ABC, abstractmethod
+
+class Notificacao(ABC):
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
+        
+    @abstractmethod
+    def enviar(self) -> bool:...
+    
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print('E-mail: Enviando - ', self.mensagem)
+        return True
+        
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print('SMS: Enviando - ', self.mensagem)
+        return True
+        
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+    if notificacao_enviada:
+        print('Notificação enviada')
+    else:
+        print('Notificação não enviada')
+        
+
+
+notificacao_email = NotificacaoEmail('TESTANDO EMAIL')
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS('TESTANDO SMS')
+notificar(notificacao_sms)
