8bbdc544e45067e11c5ea7142b80ddd92cd15deb
marina6coneto <santoscmarina@hotmail.com>
2024-03-22 13:54:58 -0300

    docstring em funções e classes

---
+++ aula150/main.py
@@ -0,0 +1,13 @@
+# import uma_linha
+#import varias_linhas
+# import varias_linhas
+#import documentando_funcoes
+import documentando_classes
+
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+#help(varias_linhas)
+#help(documentando_funcoes)
+help(documentando_classes)
