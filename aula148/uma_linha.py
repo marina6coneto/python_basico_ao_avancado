440af339dd9b887885db01716347b60fbf691680
marina6coneto <santoscmarina@hotmail.com>
2024-03-21 21:24:53 -0300

    docstring uma e varias linhas

---
+++ aula148/uma_linha.py
@@ -0,0 +1,6 @@
+"""Documentação do módulo"""
+
+variavel = 'valor'
+
+def funcao():
+    return 1
---
+++ aula148/varias_linhas.py
@@ -0,0 +1,7 @@
+"""Do not pity the dead, Harry. 
+
+
+Pity the living, and, above all, those who live without love.
+Não tenha pena dos mortos, Harry. 
+Tenha pena dos vivos, e, acima de tudo, daqueles que vivem sem amor.
+(Dumbledore - Harry Potter e as Relíquias da Morte)"""
