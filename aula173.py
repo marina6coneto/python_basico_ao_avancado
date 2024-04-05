8abffeaba19ba0e74bd3825b93a08e0ade982961
marina6coneto <santoscmarina@hotmail.com>
2024-04-05 13:54:13 -0300

    os + shutil

---
+++ aula173.py
@@ -0,0 +1,36 @@
+# os + shutil - Copiando arquivos com Python
+# Vamos copiar arquivos de uma pasta para outra.
+# Copiar -> shutil.copy
+# mover/renomear -> shutil.move
+# mover/renomear -> os.rename
+# apagar -> os.unlink
+# apagar diretorio recursivamente -> shutil.rmtree
+
+import os
+import shutil
+
+HOME = os.path.expanduser('~')
+DESKTOP = os.path.join(HOME, 'Desktop')
+PASTA_ORIGNAL = os.path.join(DESKTOP, 'desafio')
+NOVA_PASTA = os.path.join(DESKTOP, 'NOVA_PASTA')
+print(NOVA_PASTA)
+
+#shutil.rmtree(NOVA_PASTA) #nao consegui apagar pq essa pasta contem diretorio pro git 
+#apos feito isso consegueria executar o código abaixo:
+#shutil.copytree(PASTA_ORIGNAL, NOVA_PASTA) 
+shutil.move(NOVA_PASTA, NOVA_PASTA + '_EITA')
+# os.makedirs(NOVA_PASTA, exist_ok=True)
+
+# for root, dirs, files in os.walk(PASTA_ORIGNAL):
+#     for dir_ in dirs:
+#         caminho_novo_diretorio = os.path.join(
+#             root.replace(PASTA_ORIGNAL, NOVA_PASTA), dir_
+#         )
+#         os.makedirs(caminho_novo_diretorio, exist_ok=True)
+#     for file in files:
+#         caminho_arquivo = os.path.join(root, file)
+#         caminho_novo_arquivo = os.path.join(
+#             root.replace(PASTA_ORIGNAL, NOVA_PASTA), file
+#         )
+#         shutil.copy(caminho_arquivo, caminho_novo_arquivo)
+        
